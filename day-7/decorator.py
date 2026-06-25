4ba38b7bbf99399cc8a81f9c6a500cb153553bff
utkarsh <utkarsh8377@gmail.com>
2026-06-25 13:43:13 +0530

    decorator, list comprehension

---
+++ day-7/decorator.py
@@ -0,0 +1,41 @@
+# basic idea 
+
+# def decorate(function):
+#   def wrapper(*args, **kwargs):
+#     print("i am decorating this function")
+#     function(*args, **kwargs)
+  
+#   return wrapper
+
+# @decorate
+# def hello_world(person):
+#   print(f"hello {person}")
+
+# hello_world("utkarsh")
+
+
+
+# logging 
+
+def logged(function):
+  def wrapper(*args, **kwargs):
+    value = function(*args, **kwargs)
+    with open('log_file.txt','a+') as f:
+      fname = function.__name__
+      print(f"{fname} returned value {value}")
+      f.write(f"{fname} returned value {value}\n")
+    return value 
+  
+  return wrapper
+
+
+@logged
+def add(x,y):
+  return x+y
+
+print(add(10,20))
+print(add(10,20))
+print(add(10,20))
+print(add(10,20))
+
+
--- day-7/magic_method.py
+++ day-7/magic_method.py
@@ -13,7 +13,7 @@ __lt__
 __getitem__
 
 
-"""
+
 
 class Book:
   def __init__(self, title, author, num_pages):
@@ -49,3 +49,39 @@ print(B1+B2)
 print("Harry" in B2)
 
 print(B1.title)
+
+
+
+"""
+
+
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def __del__(self):
+#     print("object is being deleted")
+
+# p = Person("utkarsh", 21)
+
+
+
+class Vector:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __add__(self, other):
+    return Vector(self.x + other.x, self.y+ other.y)
+  
+  def __repr__(self):
+    return f"X: {self.x}, y: {self.y}"
+
+v1 = Vector(10,20)
+v2= Vector(30, 40)
+v3 = v1+v2
+
+print(v3.x, v3.y)
+print(v3)
