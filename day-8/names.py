2f6157a81d6a0e3384574fca87e71647527e1374
utkarsh <utkarsh8377@gmail.com>
2026-06-26 11:02:14 +0530

    i/o part 1

---
+++ day-8/names.py
@@ -0,0 +1,20 @@
+name = input("please enter your name: ")
+
+with open("names.txt", "a") as file:
+
+  file.write(f"{name}\n")
+
+
+# file.close()-> instead of close more pythonic way is to use-> with
+
+# with open("names.txt", "r") as file2:
+#   lines = file2.readlines()
+
+# for line in lines:
+#   print(f"hello, {line}", end ="")
+
+
+
+with open("names.txt", "r") as file2:
+    for line in file2:
+      print(f"hello, {line}", end="")
