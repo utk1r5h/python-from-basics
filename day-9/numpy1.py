ade9e160577bf3c1c0c673e13d414e8e2ffb47e7
utkarsh <utkarsh8377@gmail.com>
2026-06-30 09:10:44 +0530

    numpy 1

--- day-9/numpy1.py
+++ day-9/numpy1.py
@@ -1,1 +1,27 @@
 import numpy as np
+
+a = np.array([1,2,3,4,5])
+print(a)
+
+print(type(a))
+
+
+b = np.array([[1,2,3], [4,6,7], [7,8,9]])
+
+print(b[1][0])
+print(b[1,0])
+
+print(b.shape)
+print(b.ndim)
+print(b.size)
+print(b.dtype)
+
+
+c = np.array([[1,2,3], [4,"hello my name is utkarsh",7], [7,8,9]])
+print(c.dtype)
+print(type(c[1][1]))
+
+
+
+# arrays with default values 
+
